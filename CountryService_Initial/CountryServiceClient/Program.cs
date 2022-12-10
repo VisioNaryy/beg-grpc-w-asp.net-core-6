4ca879329d7990905b8f88404c129e88965480b3
Daniil Okonskiy <dofeternity@gmail.com>
2022-12-10 23:10:22 +0600

    Project template renamed

---
+++ CountryService_Initial/CountryServiceClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using CountryServiceClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        var environment = context.HostingEnvironment;
+        
+        builder.AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+        builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((_, services) =>
+    {
+        services.AddTransient<App>();
+    })
+    .Build();
+
+using (var scope = host.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    
+    try
+    {
+        await services.GetRequiredService<App>().Run();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
